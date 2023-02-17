9630c0e2385ed270cc0afa90a15449b978f55f33
twilight-warlock <shahdevansh045@gmail.com>
2023-02-17 13:08:09 +0530

    New 256 image folder created and it's code added

--- IMG_download.py
+++ IMG_download.py
@@ -2,33 +2,41 @@ import rasterio
 import os
 
 # Open the input TIF file
-with rasterio.open('input.tif') as src:
 
-    # Get the size of the input image
-    width, height = src.width, src.height
 
-    # Compute the number of 256x256 pixel images needed in the x and y directions
-    num_x = width // 256
-    num_y = height // 256
+def print():
+    k = 1
+    for z in range(1, 16):
+        with rasterio.open("./ALL_FLOOD/" + str(z) + '_P.tif') as src:
 
-    # Loop over each 256x256 pixel image
-    for i in range(num_x):
-        for j in range(num_y):
+            # Get the size of the input image
+            width, height = src.width, src.height
 
-            # Compute the window to read from the input TIF file
-            window = rasterio.windows.Window(i * 256, j * 256, 256, 256)
+            # Compute the number of 256x256 pixel images needed in the x and y directions
+            num_x = width // 256
+            num_y = height // 256
 
-            # Read the data for this window from the input TIF file
-            data = src.read(window=window)
+            # Loop over each 256x256 pixel image
+            for i in range(num_x):
+                for j in range(num_y):
 
-            # Create a new TIF file for this 256x256 pixel image
-            output_file = f"output_{i}_{j}.tif"
-            with rasterio.open(output_file, 'w', driver='GTiff', width=256, height=256, count=src.count, dtype=src.dtypes[0], crs=src.crs, transform=src.window_transform(window)) as dst:
+                    # Compute the window to read from the input TIF file
+                    window = rasterio.windows.Window(
+                        i * 256, j * 256, 256, 256)
 
-                # Write the data to the output TIF file
-                dst.write(data)
+                    # Read the data for this window from the input TIF file
+                    data = src.read(window=window)
 
-# Output a list of the output file names
-output_files = [f"output_{i}_{j}.tif" for i in range(
-    num_x) for j in range(num_y)]
-print(f"Output files: {output_files}")
+                    # Create a new TIF file for this 256x256 pixel image
+                    output_file = str(k) + "_P_256.tif"
+                    with rasterio.open("./ALL_FLOOD_256/" + output_file, 'w', driver='GTiff', width=256, height=256, count=src.count, dtype=src.dtypes[0], crs=src.crs, transform=src.window_transform(window)) as dst:
+
+                        # Write the data to the output TIF file
+                        dst.write(data)
+                    k += 1
+
+
+    # Output a list of the output file names
+    # output_files = [f"output_{i}_{j}.tif" for i in range(num_x) for j in range(num_y)]
+    # print(f"Output files: {output_files}")
+print()
